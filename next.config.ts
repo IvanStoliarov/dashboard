79dc30995a87aa620acbf6a860c731c49831d491
Ivan Stoliarov <ivan.s@absoluteweb.com>
2026-07-22 18:09:44 +0200

    chore(next): disable fetch URL logging

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   logging: {
-    fetches: {
-      fullUrl: true,
-    },
+    // fetches: {
+    //   fullUrl: true,
+    // },
   },
 };
 
